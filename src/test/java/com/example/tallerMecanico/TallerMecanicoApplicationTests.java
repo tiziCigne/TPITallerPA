a91a3b50ac4f89b14028812c6869a0a50bb0728e
tiziCigne <cignetti78@gmail.com>
2023-09-07 12:09:09 -0300

    se agrega el proyecto

---
+++ src/test/java/com/example/tallerMecanico/TallerMecanicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tallerMecanico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerMecanicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
