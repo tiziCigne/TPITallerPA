fbdc5a4f476d4573f3c865943e1042e2dae850c3
AlvaroP1017 <105307315+AlvaroP1017@users.noreply.github.com>
2023-10-09 11:34:53 -0300

    Add files via upload

--- src/main/java/com/app/web/CrudSpringBootApplication.java
+++ src/main/java/com/app/web/CrudSpringBootApplication.java
@@ -8,15 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.app.web.entidad.Cliente;
 import com.app.web.entidad.Marca;
 import com.app.web.entidad.Modelo;
-import com.app.web.entidad.OrdenTrabajo;
-import com.app.web.entidad.Servicio;
 import com.app.web.entidad.Vehiculo;
 import com.app.web.entidad.tecnico;
 import com.app.web.repository.ClienteRepositorio;
 import com.app.web.repository.MarcaRepository;
 import com.app.web.repository.ModeloRepositorio;
-import com.app.web.repository.OrdenTrabajoRepositorio;
-import com.app.web.repository.ServicioRepositorio;
 import com.app.web.repository.VehiculoRepository;
 import com.app.web.repository.tecnicoRepositorio;
 
@@ -45,25 +41,12 @@ public class CrudSpringBootApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ModeloRepositorio repositorioModelo;
-
-	@Autowired
-	private ServicioRepositorio repositorioServicio;
-	
-	@Autowired
-	private OrdenTrabajoRepositorio repositorioOrdenTrabajo;
-	
 	
 	//este método es ejecutado al iniciar la aplicación.
     //carga de datos iniciales.
 	@Override
 	public void run(String... args) throws Exception {
-	/*
-		
-		Servicio servicio1 = new Servicio("Cambio de aceite", "Cambia el aceite de un auto");
-		repositorioServicio.save(servicio1);
-		
-		Servicio servicio2 = new Servicio("Mecanica ligera", "Cambiar amortiguador");
-		repositorioServicio.save(servicio2);
+	
 		
 		Vehiculo vehiculo1 = new Vehiculo("A03 NFB");
 		repositorio.save(vehiculo1);
@@ -83,8 +66,17 @@ public class CrudSpringBootApplication implements CommandLineRunner{
 		tecnico tecnico2 = new tecnico("Pepe","Salamaleco",3585688 );	
 		repositorioTecnico.save(tecnico2);
 		
-		Cliente cliente = new Cliente("Celia","Mabel","celia69");
-		repositorioCliente.save(cliente);
+		Cliente cliente1 = new Cliente("Celia","Carranza","celi@gmail.com",784576698,"Periodistas argentinos 99","patente 455","");
+		repositorioCliente.save(cliente1);
+		
+		Cliente cliente2 = new Cliente("Alvaro","Pineda","alva@gmail.com",1515151777,"Periodistas argentinos 6984","patente 2145","");
+		repositorioCliente.save(cliente2);
+		
+		Cliente cliente3 = new Cliente("Rodrigo","Alves","rorororo@gmail.com",14785239,"Periodistas argentinos 199","patente 55","");
+		repositorioCliente.save(cliente3);
+		
+		Cliente cliente4 = new Cliente("Tiziana","Cignetti","tizi@gmail.com",985235,"Pellegrini 4578","patente 7841","");
+		repositorioCliente.save(cliente4);
 		
 		Modelo modelo1 = new Modelo("Focus", "2001");
 		repositorioModelo.save(modelo1);
@@ -94,7 +86,7 @@ public class CrudSpringBootApplication implements CommandLineRunner{
 		
 		Modelo modelo3 = new Modelo("Fiesta", "2009");
 		repositorioModelo.save(modelo3);
-*/
+		
 	}
 }
 
